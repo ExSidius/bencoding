8803515d1bb86d51d8a1adeb3790989ee9124c99
mram <mram@umd.edu>
2019-05-19 17:35:29 -0400

    Simplified testing examples

--- tests/test_decoding.py
+++ tests/test_decoding.py
@@ -1,28 +1,10 @@
 from bencoding.decode import decode
 from examples import (
 	Example,
-	STRING_EXAMPLES,
-	INTEGER_EXAMPLES,
-	LIST_EXAMPLES,
-	DICT_EXAMPLES
+	EXAMPLES,
 )
 
 
-def test_decode_string():
-	for example in STRING_EXAMPLES:
-		assert decode(example.encoded) == example.decoded
-
-
-def test_decode_integer():
-	for example in INTEGER_EXAMPLES:
-		assert decode(example.encoded) == example.decoded
-
-
-def test_decode_list():
-	for example in LIST_EXAMPLES:
-		assert decode(example.encoded) == example.decoded
-
-
-def test_decode_dict():
-	for example in DICT_EXAMPLES:
+def test_decode():
+	for example in EXAMPLES:
 		assert decode(example.encoded) == example.decoded
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,28 +1,10 @@
 from bencoding.encode import encode
 from examples import (
 	Example,
-	STRING_EXAMPLES,
-	INTEGER_EXAMPLES,
-	LIST_EXAMPLES,
-	DICT_EXAMPLES
+	EXAMPLES,
 )
 
 
-def test_encode_string():
-	for example in STRING_EXAMPLES:
-		assert encode(example.decoded) == example.encoded
-
-
-def test_encode_integer():
-	for example in INTEGER_EXAMPLES:
-		assert encode(example.decoded) == example.encoded
-
-
-def test_encode_list():
-	for example in LIST_EXAMPLES:
-		assert encode(example.decoded) == example.encoded
-
-
-def test_encode_dict():
-	for example in DICT_EXAMPLES:
+def test_encode():
+	for example in EXAMPLES:
 		assert encode(example.decoded) == example.encoded

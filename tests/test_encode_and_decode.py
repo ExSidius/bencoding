d0d97def6273d36df258f8c521aa1a7cc8ea8d9a
mram <mram@umd.edu>
2019-05-19 17:37:49 -0400

    Simplified testing infrastructure even more

--- tests/test_encode_and_decode.py
+++ tests/test_encode_and_decode.py
@@ -1,4 +1,4 @@
-from bencoding.encode import encode
+from bencoding import encode, decode
 from examples import (
 	Example,
 	EXAMPLES,
@@ -8,3 +8,8 @@ from examples import (
 def test_encode():
 	for example in EXAMPLES:
 		assert encode(example.decoded) == example.encoded
+
+
+def test_decode():
+	for example in EXAMPLES:
+		assert decode(example.encoded) == example.decoded

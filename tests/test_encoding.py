8826a135bf30bfab745eca6217d500094b7d9ba9
mram <mram@umd.edu>
2019-05-19 15:06:58 -0400

    Roughly set up more tests

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,5 +1,11 @@
-from examples import Example, STRING_EXAMPLES, INTEGER_EXAMPLES
 from bencoding.encode import encode_string, encode_integer
+from examples import (
+	Example,
+	STRING_EXAMPLES,
+	INTEGER_EXAMPLES,
+	LIST_EXAMPLES,
+	DICT_EXAMPLES
+)
 
 
 def test_encode_string():
@@ -16,7 +22,7 @@ def test_encode_integer():
 # 	for example in INTEGER_EXAMPLES:
 # 		assert encode_list(example.decoded) == example.encoded
 #
-# 		
+#
 # def test_encode_dict():
 # 	for example in INTEGER_EXAMPLES:
 # 		assert encode_dict(example.decoded) == example.encoded

fb7b0e63aa9fab015156784453d7599fdbf242a1
mram <mram@umd.edu>
2019-05-19 14:15:59 -0400

    updated gitignore

---
+++ bencoding/tests/test_sanity.py
@@ -0,0 +1,12 @@
+def test_addition():
+	"""
+	Bertrand Russell would have wanted this test.
+	"""
+	assert 1 + 1 == 2
+
+
+def test_baddition():
+	"""
+	George Orwell would have wanted this one.
+	"""
+	assert 2 + 2 != 5

0a61c4548ab78ea484566f09e4ee85a245ab0cc7
mram <mram@umd.edu>
2019-05-20 17:08:40 -0400

    Switch encoding to bytes

--- bencoding/encode.py
+++ bencoding/encode.py
@@ -2,58 +2,25 @@ from typing import Union, List, Dict
 
 
 def encode_bytes(value: bytes) -> bytes:
-	return f'{len(value)}'.encode() + b':' + value
-
-
-_encode_bytes = encode_bytes
-
-
-def _encode_string(value: str) -> str:
-	return f'{len(value)}:{value}'
+	return b'%d:%b' % (len(value), value)
 
 
 def encode_string(value: str) -> bytes:
-	return _encode_string(value).encode()
-
-
-def _encode_integer(value: int) -> str:
-	return f'i{value}e'
+	return f'{len(value)}:{value}'.encode()
 
 
 def encode_integer(value: int) -> bytes:
-	return _encode_integer(value).encode()
-
-
-def _encode_list(value: List[Union[str, int, List, Dict]]) -> str:
-	return f"l{''.join([_encode(v) for v in value])}e"
+	return b'i%de' % value
 
 
 def encode_list(value: List[Union[str, int, List, Dict]]) -> bytes:
-	return _encode_list(value).encode()
-
-
-def _encode_dict(value: Dict[str, Union[str, int, List, Dict]]) -> str:
-	return f"d{''.join([f'{len(k)}:{k}{_encode(v)}' for k, v in value.items()])}e"
+	return b'l%be' % b''.join(encode(v) for v in value)
 
 
 def encode_dict(value: Dict[str, Union[str, int, List, Dict]]) -> bytes:
-	value = {k: value[k] for k in sorted(value)}
-	return _encode_dict(value).encode()
-
-
-def _encode(value: Union[str, int, List, Dict]) -> Union[str, bytes]:
-	if isinstance(value, str):
-		return _encode_string(value)
-	elif isinstance(value, int):
-		return _encode_integer(value)
-	elif isinstance(value, list):
-		return _encode_list(value)
-	elif isinstance(value, dict):
-		return _encode_dict(value)
-	elif isinstance(value, bytes):
-		return _encode_bytes(value)
-	else:
-		raise Exception(f'{type(value)} is an unsupported type.')
+	value = {k:value[k] for k in sorted(value)}
+	return b'd%be' % b''.join(b'%b%b' % (encode_string(k), encode(v))
+	                          for k, v in value.items())
 
 
 def encode(value: Union[str, int, List, Dict]) -> bytes:

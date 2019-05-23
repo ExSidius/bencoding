19f4ec8955475067a5cc8d1f180b7163b05a5853
mram <mram@umd.edu>
2019-05-23 12:53:15 -0400

    Speed test

--- tests/test_encode_and_decode.py
+++ tests/test_encode_and_decode.py
@@ -1,13 +1,33 @@
+import time
+from statistics import mean, stdev
+
 from bencoding import encode, decode
 from examples import (
 	Example,
 	EXAMPLES,
+	encoded,
+	decoded,
 )
 
 
 def test_encode():
 	for example in EXAMPLES:
 		assert encode(example.decoded) == example.encoded
+
+
+def test_encode_speed():
+	times = []
+	for i in range(100000):
+		start = time.time()
+		encode(decoded)
+		end = time.time()
+		times.append(end - start)
+
+	agg = mean(times)
+	dev = stdev(times)
+
+	assert agg < 2.1e-05
+	assert dev < 3.3e-06
 
 
 def test_decode():

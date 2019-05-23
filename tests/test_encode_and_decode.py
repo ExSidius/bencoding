f6e561560b6b26e9bb5eda24c0bf8e5bde1cf1fb
mram <mram@umd.edu>
2019-05-23 13:04:36 -0400

    Removing speed tests

--- tests/test_encode_and_decode.py
+++ tests/test_encode_and_decode.py
@@ -15,36 +15,6 @@ def test_encode():
 		assert encode(example.decoded) == example.encoded
 
 
-def test_encode_speed():
-	times = []
-	for i in range(100000):
-		start = time.time()
-		encode(decoded)
-		end = time.time()
-		times.append(end - start)
-
-	agg = mean(times)
-	dev = stdev(times)
-
-	assert agg < 3.0e-05
-	assert dev < 7.0e-06
-
-
 def test_decode():
 	for example in EXAMPLES:
 		assert decode(example.encoded) == example.decoded
-
-
-def test_decode_speed():
-	times = []
-	for i in range(10000):
-		start = time.time()
-		decode(encoded)
-		end = time.time()
-		times.append(end - start)
-
-	agg = mean(times)
-	dev = stdev(times)
-
-	# assert agg < 2.1e-05
-	# assert dev < 3.3e-06
